cfb1e459a6e9f80d4f8890583022b84b421af7d2
Aakash Virdhe <avirdhe.virdhe@gmail.com>
2026-01-14 18:26:27 +0530

    Removed irrelevant files

--- public/assets/js/motionWorker.js
+++ public/assets/js/motionWorker.js
@@ -11,55 +11,64 @@ var interpreter;
 var machineType;
 
 
-function init(header) {
-	parser = new CWS.Parser();
+function init (header) 
+{
+	parser 		= new CWS.Parser();
 	interpreter = new CWS.Interpreter(header.machine);
 }
 
-function runCode(data) {
+function runCode (data) 
+{
 	var code = data.code;
 	var errList = [];
-	try {
-		parser.parseCode(code);
-	} catch (e) {
+	try 
+	{
+		parser.parseCode(code);	
+	} catch(e) 
+	{
 		errList.push(String(e));
 	}
-	while (cmd = parser.getCommand()) {
-		try {
+	while(cmd=parser.getCommand())
+	{
+		try 
+		{
 			interpreter.runCommand(cmd);
-		}
-		catch (e) {
+		} 
+		catch(e) 
+		{
 			errList.push(String(e));
 		}
 	}
 
-	var l = interpreter.outputCommands.length * 2 * 3;
-	var positions = new Float32Array(l);
-	var color = new Float32Array(interpreter.outputCommands.length * 2);
-	var i = 0;
-	var c = 0;
+	var l = interpreter.outputCommands.length*2*3;
+	var positions = new Float32Array( l );
+	var color = new Float32Array( interpreter.outputCommands.length*2);
+	var i=0;
+	var c=0;
+	
+	while (i<l)
+	{
+		var cmd=interpreter.getCommand();
 
-	while (i < l) {
-		var cmd = interpreter.getCommand();
+		positions[ i + 0 ] = cmd.x0;
+		positions[ i + 1 ] = cmd.y0;
+		positions[ i + 2 ] = cmd.z0;
 
-		positions[i + 0] = cmd.x0;
-		positions[i + 1] = cmd.y0;
-		positions[i + 2] = cmd.z0;
+		positions[ i + 3 ] = cmd.x1;
+		positions[ i + 4 ] = cmd.y1;
+		positions[ i + 5 ] = cmd.z1;
 
-		positions[i + 3] = cmd.x1;
-		positions[i + 4] = cmd.y1;
-		positions[i + 5] = cmd.z1;
+		color[c]   = cmd.ctype;
+		color[c+1] = cmd.ctype;
 
-		color[c] = cmd.ctype;
-		color[c + 1] = cmd.ctype;
-
-		c += 2;
-		i += 6;
+		c+=2;
+		i+=6;
 	}
-	return { positions: positions, color: color, error: errList };
+	return {positions:positions,color:color,error:errList};
 }
 
-onmessage = function (ev) {
+onmessage = function (ev) 
+{
 	init(ev.data.header);
 	var result;
 	result = runCode(ev.data);
