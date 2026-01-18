da7bc9cbe589ccdb1754bf6444b386df02e7149a
Aakash Virdhe <aakash.virdhe@gmail.com>
2026-01-18 13:16:49 +0530

    feat: Initialize React application with main layout and core UI components for editor, canvas, and navigation.

---
+++ src/components/CodeEditor.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+
+const CodeEditor: React.FC = () => {
+    return (
+        <div id="editor">
+        </div>
+    );
+};
+
+export default CodeEditor;
---
+++ src/components/TopBar.tsx
@@ -0,0 +1,46 @@
+
+import React from 'react';
+
+const TopBar: React.FC = () => {
+    return (
+        <div id="topMenu">
+            <nav>
+                <ul>
+                    <li>
+                        <div><span title="File" className="icon icon-folder-open"></span>File</div>
+                        <ul>
+                            <li>
+                                <div title="New Project">New</div>
+                            </li>
+                            <li>
+                                <div title="Open Project">Open</div>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <div><span title="Machine" className="icon icon-cogs"></span>Machine</div>
+                        <ul>
+                            <li>
+                                <div title="Open Machine">Open Machine</div>
+                            </li>
+                            <li>
+                                <div title="Tool">Tool</div>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <div><span title="Workpiece" className="icon icon-codepen"></span>Workpiece</div>
+                        <ul>
+                            <li>
+                                <div title="Workpiece dimensions">Dimensions</div>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+            <span id="machineIcon"></span>
+        </div>
+    );
+};
+
+export default TopBar;
